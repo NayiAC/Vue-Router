ffd2627479416f9fb845660dc02759866fb67ab6
Nayarett <nayarett.castillo@gmail.com>
2024-09-04 23:07:43 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Navbar />
+  
+  <router-view/>
+  
+  <medios-pago/>
+  <Footer />
+</template>
+
+<script>
+import Navbar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+import MediosPago from '@/components/MediosPago.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Navbar,
+    Footer,
+    MediosPago
+  }
+}
+</script>
